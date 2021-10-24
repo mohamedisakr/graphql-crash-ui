5a200d2c7affaf07bbaba18b095c0c312b742502
mohamedisakr <mohamedisakr@gmail.com>
2021-10-24 13:07:46 +0200

    using query variables

--- src/pages/AnimalPage/AnimalPage.js
+++ src/pages/AnimalPage/AnimalPage.js
@@ -1,57 +1,78 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import animals from "../../assets/images"
-import star from "../../assets/svg/star.svg"
-import "./AnimalPage.css"
-import { useParams } from "react-router-dom"
+import {useQuery, gql} from '@apollo/client'
+import {Container} from 'react-bootstrap'
+import animals from '../../assets/images'
+import star from '../../assets/svg/star.svg'
+import './AnimalPage.css'
+import {useParams} from 'react-router-dom'
+
+const ANIMAL_VIEW_QUERY = gql`
+  query GetAnimal($slug: String!) {
+    animal(slug: $slug) {
+      id
+      image
+      title
+      price
+      description
+      stock
+    }
+  }
+`
 
 function AnimalPage() {
+  const {slug} = useParams()
+  const {loading, error, data} = useQuery(ANIMAL_VIEW_QUERY, {
+    variables: {slug},
+  })
 
-    const { slug } = useParams();
-    
-
-    return (
-        <div className="py-5">
-            <Container>
-                <div className="d-flex">
-                    <img className="product-img"  style={{marginRight: "1rem"}}/>
-                <div className="text-container">
-                        <h1>{}</h1>
-                        <div className="star-container">
-                            <img src={star}/>
-                            <img src={star}/>
-                            <img src={star}/>
-                            <img src={star}/>
-                            <img src={star}/>
-                            <div className="rating-stock-container">
-                                <p>1402 rating</p>
-                                <p>{} in stock</p>
-                            </div>
-                            
-                        </div>
-                        <div className="about-container">
-                            <h4>About this Animal</h4>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                            <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                            <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
-                            <li>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-                        </div>
-                    </div>
-                    <div className="cart-container border">
-                        <p className="price"><span>CAD$ {}</span></p>
-                        <p className="delivery-time" >FREE delivery: Thursday, Feb 25 Details
-                            <button className="buy-now-btn" style={{marginTop: "2rem"}}>
-                                Add to Cart
-                            </button>
-                            <button>
-                                Buy Now
-                            </button>
-                        </p>
-                    </div>
-                </div>
-            </Container>
+  if (loading) return <div>loading...</div>
+  if (error) return <div>error has occurred</div>
+  const {id, image, title, price, description, stock} = data.animal
+  return (
+    <div className="py-5">
+      <Container>
+        <div key={id} className="d-flex">
+          <img
+            src={animals[image]}
+            className="product-img"
+            style={{marginRight: '1rem'}}
+          />
+          <div className="text-container">
+            <h1>{title}</h1>
+            <div className="star-container">
+              <img src={star} />
+              <img src={star} />
+              <img src={star} />
+              <img src={star} />
+              <img src={star} />
+              <div className="rating-stock-container">
+                <p>1402 rating</p>
+                <p>{stock} in stock</p>
+              </div>
+            </div>
+            <div className="about-container">
+              <h4>About this Animal</h4>
+              {description.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </div>
+          </div>
+          <div className="cart-container border">
+            <p className="price">
+              <span>CAD$ {price}</span>
+            </p>
+            <p className="delivery-time">
+              FREE delivery: Thursday, Feb 25 Details
+              <button className="buy-now-btn" style={{marginTop: '2rem'}}>
+                Add to Cart
+              </button>
+              <button>Buy Now</button>
+            </p>
+          </div>
         </div>
-    )
+      </Container>
+    </div>
+  )
 }
 
 export default AnimalPage
--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,17 +1,42 @@
 import React from 'react'
+import {useQuery, gql} from '@apollo/client'
 import {useParams} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import CardDisplay from '../components/CardDisplay/CardDisplay'
 
+const CATEGORY_WITH_ANIMALS = gql`
+  query GetCategoryWithAnimals($slug: String!) {
+    category(slug: $slug) {
+      id
+      image
+      category
+      slug
+      animals {
+        id
+        title
+        price
+        image
+      }
+    }
+  }
+`
+
 function CategoryPage() {
   const {slug} = useParams()
+  const {loading, error, data} = useQuery(CATEGORY_WITH_ANIMALS, {
+    variables: {slug},
+  })
 
+  if (loading) return <div>loading...</div>
+  if (error) return <div>error has occurred</div>
+
+  const {category, animals} = data.category
   return (
     <div className="py-5">
       <Container>
         <h1 className="text-capitalize">
-          {}
-          <CardDisplay animals={[]} />
+          {category}
+          <CardDisplay animals={animals} />
         </h1>
       </Container>
     </div>
