197add592a678d6f527ec908d8b25c1f59568198
mohamedisakr <mohamedisakr@gmail.com>
2021-10-24 10:58:37 +0200

    populating categories component

--- src/components/CategoryDisplay/CategoryDisplay.jsx
+++ src/components/CategoryDisplay/CategoryDisplay.jsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react'
-import "./CategoryDisplay.css"
-import { Container } from 'react-bootstrap'
+import React, {useState} from 'react'
+import {useQuery, gql} from '@apollo/client'
+import './CategoryDisplay.css'
+import {Container} from 'react-bootstrap'
 import animals from '../../assets/images'
 
-import { Link } from "react-router-dom"
+import {Link} from 'react-router-dom'
 
+const CATEGORIES_QUERY = gql`
+  query GetCategories {
+    categories {
+      id
+      image
+      category
+      slug
+    }
+  }
+`
 
 function CategoryDisplay() {
+  const {loading, error, data} = useQuery(CATEGORIES_QUERY)
 
-    return (
-        <div className="CategoryDisplay">
-            <Container className="CategoryDisplay-container">
-                {[].map(category => {
-                    return (
-                        <Link to={`/products/${category.slug}`} className="CategoryDisplay-card-container">
-                            <div className="CategoryDisplay-card">
-                                <img src={animals[category.img]} /> 
-                            </div>
-                            <h3>{category.category}</h3>
-                        </Link>
-                    )
-                })}
-            </Container>
-        </div>
-    )
+  if (loading) return <div>loading...</div>
+  if (error) return <div>error has occurred</div>
+  return (
+    <div className="CategoryDisplay">
+      <Container className="CategoryDisplay-container">
+        {data.categories.map(({category, slug, image}) => {
+          return (
+            <Link
+              to={`/products/${slug}`}
+              className="CategoryDisplay-card-container"
+            >
+              <div className="CategoryDisplay-card">
+                <img src={animals[image]} />
+              </div>
+              <h3>{category}</h3>
+            </Link>
+          )
+        })}
+      </Container>
+    </div>
+  )
 }
 
 export default CategoryDisplay
--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,24 +1,21 @@
 import React from 'react'
-import { useParams } from "react-router-dom"
-import { Container } from 'react-bootstrap'
+import {useParams} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
 import CardDisplay from '../components/CardDisplay/CardDisplay'
 
 function CategoryPage() {
-    
-    const { slug } = useParams()
+  const {slug} = useParams()
 
-    return (
-        <div className="py-5">
-            <Container>
-                <h1 className="text-capitalize">
-                    {}
-                    <CardDisplay 
-                        animals={[]}
-                    />
-                </h1>
-            </Container>
-        </div>
-    )
+  return (
+    <div className="py-5">
+      <Container>
+        <h1 className="text-capitalize">
+          {}
+          <CardDisplay animals={[]} />
+        </h1>
+      </Container>
+    </div>
+  )
 }
 
 export default CategoryPage
