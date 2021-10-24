5e31a375909f4babed6a1da77d6d863a1d569412
mohamedisakr <mohamedisakr@gmail.com>
2021-10-24 09:42:56 +0200

    populating hero component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './components/Nav/Nav';
-import { Switch, Route, BrowserRouter } from "react-router-dom"
-import LandingPage from './pages/LandingPage';
-import CategoryPage from './pages/CategoryPage';
-import AnimalPage from './pages/AnimalPage/AnimalPage';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client' // import logo from './logo.svg';
+import './App.css'
+import Nav from './components/Nav/Nav'
+import {Switch, Route, BrowserRouter} from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import CategoryPage from './pages/CategoryPage'
+import AnimalPage from './pages/AnimalPage/AnimalPage'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
           <Nav />
           <Switch>
-            <Route exact strict path="/" component={LandingPage}/>
-            <Route exact strict path="/products/:slug" component={CategoryPage}/>
-            <Route exact strict path="/product/:slug" component={AnimalPage}/>
+            <Route exact strict path="/" component={LandingPage} />
+            <Route
+              exact
+              strict
+              path="/products/:slug"
+              component={CategoryPage}
+            />
+            <Route exact strict path="/product/:slug" component={AnimalPage} />
           </Switch>
-      </BrowserRouter>
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/MainHero/MainHero.js
+++ src/components/MainHero/MainHero.js
@@ -1,30 +1,43 @@
 import React from 'react'
-import "./MainHero.css"
-import animals from "../../assets/images"
-import {Container} from "reactstrap"
+import {useQuery, gql} from '@apollo/client'
+import './MainHero.css'
+import animals from '../../assets/images'
+import {Container} from 'reactstrap'
 
 function MainHero() {
+  const {loading, error, data} = useQuery(gql`
+    {
+      mainCards {
+        title
+        image
+      }
+    }
+  `)
 
-    return (
-        <div className="MainHero">
-            <Container>
-                    <div className="header-container">
-                        <h2>Find your <br/> new four-legged <br/> best friend</h2>
-                        <img src={animals.rhino} />
-                    </div>
-                    <div className="cards-container">
-                        {[].map(card => {
-                            return (
-                                <div className="card">
-                                    <h3>{card.title}</h3>
-                                    <img src={animals[card.img]} style={{width: "100%"}}/>
-                                </div>
-                            )
-                        })}
-                    </div>
-            </Container>
+  if (loading) return <div>loading...</div>
+  if (error) return <div>error has occurred</div>
+  return (
+    <div className="MainHero">
+      <Container>
+        <div className="header-container">
+          <h2>
+            Find your <br /> new four-legged <br /> best friend
+          </h2>
+          <img src={animals.rhino} />
         </div>
-    )
+        <div className="cards-container">
+          {data.mainCards.map((card, index) => {
+            return (
+              <div key={index} className="card">
+                <h3>{card.title}</h3>
+                <img src={animals[card.image]} style={{width: '100%'}} />
+              </div>
+            )
+          })}
+        </div>
+      </Container>
+    </div>
+  )
 }
 
 export default MainHero
